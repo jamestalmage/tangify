1c589fdefade02a02672177f9b6292625396cfc1
James Talmage <james.talmage@jrtechnical.com>
2015-02-26 23:37:15 -0500

    sourcemap support!

--- example/src/add.coffee
+++ example/src/add.coffee
@@ -1,5 +1,3 @@
-add = angular.module "math.add", []
+module.exports = (add = angular.module "math.add", []).name
 
 add.value 'add', (lhs, rhs) -> lhs + rhs
-
-module.exports = add.name
--- index.js
+++ index.js
@@ -3,39 +3,29 @@
 var through      = require('through2');
 var transformify = require('transformify');
 var transform    = require('ng-test-utils/src/ngInject');
-var convert = require('convert-source-map');
+var convert      = require('convert-source-map');
 
-//module.exports =
-
-  function oldway (file, options) {
-  if (/\.json$/.test(file)) return through();
-  return transformify(doTransform)();
+module.exports = function (file, opts) {
+  if(/\.json$/.test(file)) return through();
+  return transformify(injectAnnotatedDependencies(file, opts))();
 };
 
-
-function injectify(file){
-  if(isJson(file)) return through();
-
-  var data = '', stream = through(write, end);
-
-  return stream;
-
-  function write(buf, encoding, callback){
-    data += buf;
-    callback();
-  }
-
-  function end(cb){
-    var result = transform(data, {sourceFileName:file});
-    var map = convert.fromObject(result.map);
-    map.setProperty('sources',[file]);
-    this.push(result.code + '\n' + map.toComment() + '\n');
-    cb();
+function injectAnnotatedDependencies(file, opts){
+  return function (contents){
+    if(opts._flags.debug){
+      var inputSourceMap = convert.fromSource(contents);
+      if(inputSourceMap) {
+        inputSourceMap = inputSourceMap.toObject();
+      }
+      var result = transform(contents, {
+        sourceFileName: file,
+        inputSourceMap: inputSourceMap
+      });
+      var map = convert.fromObject(result.map);
+      return result.code + '\n' + map.toComment() + '\n';
+    }
+    else {
+      return transform(contents).code;
+    }
   }
 }
-
-function isJson(file){
-  return /\.json$/.test(file);
-}
-
-module.exports = injectify;
