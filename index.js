7883f9e691bdf7d156a6130a8d405f6844236c8f
James Talmage <james.talmage@jrtechnical.com>
2015-03-03 00:55:34 -0500

    updating to latest ng-test-utils

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var through      = require('through2');
 var transformify = require('transformify');
 var transform    = require('ng-test-utils');
-var convert      = require('convert-source-map');
 
 module.exports = function (file, opts) {
   if(/\.json$/.test(file)) return through();
@@ -13,16 +12,11 @@ module.exports = function (file, opts) {
 function injectAnnotatedDependencies(file, opts){
   return function (contents){
     if(opts._flags.debug){
-      var inputSourceMap = convert.fromSource(contents);
-      if(inputSourceMap) {
-        inputSourceMap = inputSourceMap.toObject();
-      }
-      var result = transform(contents, {
-        sourceFileName: file,
-        inputSourceMap: inputSourceMap
-      });
-      var map = convert.fromObject(result.map);
-      return result.code + '\n' + map.toComment() + '\n';
+      return transform(contents, {
+        sourceMap:true,
+        sourceFileName:file,
+        appendSourceMapComment:true
+      }).code;
     }
     else {
       return transform(contents).code;
